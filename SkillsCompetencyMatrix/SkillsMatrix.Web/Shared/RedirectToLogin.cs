8e10761b3331961c11cdb4a2af274efceb03a622
Mashudu Nemukula <mnemukula@solugrowth.com>
2020-10-18 23:08:28 +0200

    added auth

---
+++ SkillsCompetencyMatrix/SkillsMatrix.Web/Shared/RedirectToLogin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsMatrix.Web.Shared
+{
+    public class RedirectToLogin: ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            NavigationManager.NavigateTo("/signin");
+            return base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
